76ec79e5b735a952449b38785ac895505dcd6562
joachim <47057544+joachimasare@users.noreply.github.com>
2023-10-13 00:05:42 -0400

    viz_showing_
    viz_showing but small window

--- static/js/app.js
+++ static/js/app.js
@@ -1,84 +1,99 @@
-const width = window.innerWidth;
-const height = window.innerHeight * 0.85;
+const svg = d3.select("#graph");
+const width = svg.attr("width");
+const height = svg.attr("height");
 
-const svg = d3.select("body")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
-
-// Fetch data from the Flask API
-d3.json("/occupations").then(data => {
-    // Assuming each occupation will be linked to several others, we need to prepare our data for a force graph.
-    // We will make dummy links for the demo. In real use, you would likely use real connections or other criteria.
-    const nodes = data;
-    const links = [];
-
-    nodes.forEach((source, i) => {
-        nodes.forEach((target, j) => {
-            if (i !== j) {
-                links.push({
-                    source,
-                    target
-                });
-            }
-        });
-    });
-
-    const simulation = d3.forceSimulation(nodes)
-        .force("link", d3.forceLink(links).id(d => d.id))
-        .force("charge", d3.forceManyBody())
-        .force("center", d3.forceCenter(width / 2, height / 2));
-
-    const link = svg.append("g")
-        .attr("class", "links")
-        .selectAll("line")
-        .data(links)
-        .enter().append("line")
-        .attr("stroke-width", 1);
-
-    const node = svg.append("g")
-        .attr("class", "nodes")
-        .selectAll("circle")
-        .data(nodes)
-        .enter().append("circle")
-        .attr("r", 5)
-        .attr("fill", "white")
-        .call(d3.drag()
-            .on("start", dragstarted)
-            .on("drag", dragged)
-            .on("end", dragended));
-
-    node.append("title")
-        .text(d => d['Occupational Title']);
-
-    simulation.nodes(nodes).on("tick", ticked);
-    simulation.force("link").links(links);
-
-    function ticked() {
-        link.attr("x1", d => d.source.x)
-            .attr("y1", d => d.source.y)
-            .attr("x2", d => d.target.x)
-            .attr("y2", d => d.target.y);
-
-        node.attr("cx", d => d.x)
-            .attr("cy", d => d.y);
-    }
-    
+function drag(simulation) {
     function dragstarted(event, d) {
         if (!event.active) simulation.alphaTarget(0.3).restart();
         d.fx = d.x;
         d.fy = d.y;
     }
-    
+
     function dragged(event, d) {
         d.fx = event.x;
         d.fy = event.y;
     }
-    
+
     function dragended(event, d) {
         if (!event.active) simulation.alphaTarget(0);
         d.fx = null;
         d.fy = null;
     }
-});
 
+    return d3.drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended);
+}
+
+
+// Fetch all occupations first
+d3.json("/occupations").then(function(allOccupations) {
+    const nodes = allOccupations.map(occupation => ({
+        id: occupation["SOC Code"],
+        title: occupation["Occupational Title"],
+        group: occupation["SOC Group"]
+    }));
+
+    // To store all the links
+    let links = [];
+
+    // Fetch similarity links for each occupation
+    const promises = nodes.map(node => 
+        d3.json(`/similar_occupations/${node.id}`).then(similarOccupations => {
+            similarOccupations.forEach(related => {
+                links.push({
+                    source: node.id,
+                    target: related["SOC Code"],
+                    value: related["Similarity Score"]
+                });
+            });
+        })
+    );
+
+    // Once all similar occupations are fetched and links are formed
+    Promise.all(promises).then(() => {
+        const simulation = d3.forceSimulation(nodes)
+            .force("link", d3.forceLink(links).id(d => d.id))
+            .force("charge", d3.forceManyBody().strength(-400))
+            .force("center", d3.forceCenter(width / 2, height / 2));
+
+        const link = svg.append("g")
+            .attr("stroke", "#999")
+            .attr("stroke-opacity", 0.6)
+            .selectAll("line")
+            .data(links)
+            .join("line")
+            .attr("stroke-width", d => Math.sqrt(d.value) * 2); // Adjust stroke width factor as per preference
+
+        const node = svg.append("g")
+            .attr("stroke", "#fff")
+            .attr("stroke-width", 1.5)
+            .selectAll("circle")
+            .data(nodes)
+            .join("circle")
+            .attr("r", 5)
+            .attr("fill", "#69b3a2")
+            .on("mouseover", d => {
+                // Add code to show tooltip with d.title and other details if needed
+            })
+            .on("mouseout", d => {
+                // Add code to hide tooltip
+            })
+            .call(drag(simulation));
+
+        simulation.on("tick", () => {
+            link.attr("x1", d => d.source.x)
+                .attr("y1", d => d.source.y)
+                .attr("x2", d => d.target.x)
+                .attr("y2", d => d.target.y);
+
+            node.attr("cx", d => d.x)
+                .attr("cy", d => d.y);
+        });
+
+        svg.node().addEventListener("dblclick", () => {
+            if (simulation.alpha() < 0.1) simulation.restart();
+        });
+    });
+});
