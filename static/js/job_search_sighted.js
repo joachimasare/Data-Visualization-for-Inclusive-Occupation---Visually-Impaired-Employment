93192a529c3aaf562c3cda4151016f0fbcda6d46
joachim <47057544+joachimasare@users.noreply.github.com>
2023-10-14 06:32:30 -0400

    Update job_search_sighted.js
    added job sighted mode but circles moving away from center and not toward

--- static/js/job_search_sighted.js
+++ static/js/job_search_sighted.js
@@ -1,8 +1,33 @@
 const svg = d3.select("#sightedgraph")
     .attr("width", window.innerWidth)
-    .attr("height", window.innerHeight - 64 - 32);
+    .attr("height", window.innerHeight - 64 - 32)
+    .attr("viewBox", [0, 0, window.innerWidth, window.innerHeight - 64 - 32]);
 
-svg.attr("viewBox", [0, 0, window.innerWidth, window.innerHeight - 64 - 32]);
+// Create a container for the visualization
+const container = svg.append("g");
+
+// Set up the zoom behavior
+const zoomBehavior = d3.zoom()
+    .scaleExtent([0.5, 5])  // The range of zoom scaling (min and max zoom scale)
+    .on("zoom", zoomed);
+
+// Add zoom behavior to the SVG
+svg.call(zoomBehavior);
+
+function zoomed({ transform }) {
+    container.attr("transform", transform);
+}
+
+// Rest of the code remains mostly unchanged...
+const tooltip = d3.select("body")
+    .append("div")
+    .style("position", "absolute")
+    .style("z-index", "10")
+    .style("background", "white")
+    .style("border", "1px solid #ccc")
+    .style("padding", "5px")
+    .style("visibility", "hidden")
+    .text("Tooltip");
 
 d3.json("/occupations").then(function(allOccupations) {
     const nodes = allOccupations.map(occupation => ({
@@ -11,75 +36,82 @@ d3.json("/occupations").then(function(allOccupations) {
         group: occupation["SOC Group"],
         blind_work: occupation["Blind Employed"]
     }));
+    
+    // Create a force simulation for the occupation circles
+    const simulation = d3.forceSimulation(nodes)
+        .force("center", d3.forceCenter(window.innerWidth / 2, (window.innerHeight - 64 - 32) / 2))
+        .force("collide", d3.forceCollide().radius(5)) // Radius of 5 to avoid overlapping
+        .on("tick", ticked);
 
-    // Group data by SOC Group for clustering
-    let groupedData = d3.group(nodes, d => d.group);
-
-    let clusterCentroids = new Map();
-    groupedData.forEach((values, key) => {
-        clusterCentroids.set(key, calculateCentroid(values));
-    });
-
-    // Create circles for each occupation
-    svg.selectAll("circle")
-        .data(nodes)
-        .enter().append("circle")
-        .attr("r", 5)
-        .attr("fill", d => d.blind_work ? "#CDFF64" : "#585858");
-
-    // Add circles around clusters
-    svg.selectAll(".cluster-circle")
-        .data(Array.from(groupedData.keys()))
-        .enter().append("circle")
-        .attr("class", "cluster-circle")
-        .attr("r", 30) // Adjust the radius as needed
-        .attr("cx", d => clusterCentroids.get(d)[0])
-        .attr("cy", d => clusterCentroids.get(d)[1])
-        .attr("fill", "none")
-        .attr("stroke", "#444")
-        .attr("stroke-width", 2);
-
-    let simulation = d3.forceSimulation(nodes)
-        .force("x", d3.forceX(d => clusterCentroids.get(d.group)[0]).strength(0.5))
-        .force("y", d3.forceY(d => clusterCentroids.get(d.group)[1]).strength(0.5))
-        .force("collide", d3.forceCollide(10)) 
-        .force("cluster", forceCluster());
-
-    simulation.on("tick", () => {
-        svg.selectAll("circle")
+    function ticked() {
+        svg.selectAll(".occupation")
             .attr("cx", d => d.x)
             .attr("cy", d => d.y);
-    });
-
-    function calculateCentroid(values) {
-        // Calculate an average position for initial cluster centroids
-        let x = 0, y = 0;
-        values.forEach(d => {
-            x += d.x;
-            y += d.y;
-        });
-        return [x/values.length, y/values.length];
     }
 
-    function forceCluster() {
-        const strength = 0.2;
-        return (alpha) => {
-            nodes.forEach(d => {
-                const cluster = clusterCentroids.get(d.group);
-                if (cluster) {
-                    d.vx -= (d.x - cluster[0]) * strength * alpha;
-                    d.vy -= (d.y - cluster[1]) * strength * alpha;
-                }
-            });
-        };
-    }
-
-    // Add SOC Group names
-    groupedData.forEach((value, key) => {
-        let centroid = clusterCentroids.get(key);
-        svg.append("text")
-           .attr("x", centroid[0])
-           .attr("y", centroid[1] - 10)  // slight shift upwards for better visibility
-           .text(key);
-    });
+    visualizeData(container, nodes);
 });
+
+
+function visualizeData(svg, occupationData) {
+    let groupedOccupations = d3.group(occupationData, d => d.group);
+
+    let clusterData = Array.from(groupedOccupations, ([name, values]) => ({
+        name: name,
+        occupations: values,
+        size: values.length
+    }));
+
+    const numOfColumns = Math.ceil(Math.sqrt(clusterData.length));
+    const horizontalSpacing = window.innerWidth / numOfColumns;
+    const verticalSpacing = (window.innerHeight - 64 - 32) / numOfColumns;
+
+    clusterData.forEach((cluster, i) => {
+        cluster.cx = (i % numOfColumns + 0.5) * horizontalSpacing;
+        cluster.cy = (Math.floor(i / numOfColumns) + 0.5) * verticalSpacing;
+        cluster.radius = 50 + (cluster.size / 873) * 150;
+    });
+
+    // Drawing the cluster circles
+    svg.selectAll(".cluster")
+       .data(clusterData)
+       .enter()
+       .append("circle")
+       .attr("class", "cluster")
+       .attr("cx", d => d.cx)
+       .attr("cy", d => d.cy)
+       .attr("r", d => d.radius)
+       .attr("fill", "none") // No fill to make it transparent
+       .attr("stroke", "#585858") // Stroke color for the circumference
+       .attr("stroke-width", 2);
+
+    // Positioning the occupation circles around their respective cluster circle
+    occupationData.forEach((occupation) => {
+        let cluster = clusterData.find(c => c.name === occupation.group);
+        let theta = (Math.random() * 2 * Math.PI);
+        occupation.x = cluster.cx + Math.cos(theta) * (cluster.radius + 10);
+        occupation.y = cluster.cy + Math.sin(theta) * (cluster.radius + 10);
+    });
+
+    // Drawing the occupation circles
+    svg.selectAll(".occupation")
+       .data(occupationData)
+       .enter()
+       .append("circle")
+       .attr("class", "occupation")
+       .attr("cx", d => d.x)
+       .attr("cy", d => d.y)
+       .attr("r", 4.5)
+       .attr("fill", d => d.blind_work ? "#CDFF64" : "#585858")
+       .on("mouseover", function(event, d) { // Mouseover event
+           tooltip.text(d.title) // Set the tooltip text to the occupation's title
+               .style("visibility", "visible");
+       })
+       .on("mousemove", function(event, d) { // Mousemove event
+           tooltip.style("top", (event.pageY - 10) + "px")
+               .style("left", (event.pageX + 10) + "px");
+       })
+       .on("mouseout", function(d) { // Mouseout event
+           tooltip.style("visibility", "hidden");
+       });
+}
